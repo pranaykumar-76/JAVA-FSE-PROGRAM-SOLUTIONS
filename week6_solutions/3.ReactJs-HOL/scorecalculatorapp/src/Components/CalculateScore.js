122d58494578b381969668235a2dd9db01756010
G.Pranay Kumar <pranaykumar05705@gamil.com>
2025-07-27 11:45:08 +0530

    week6 files uploaded

---
+++ week6_solutions/3.ReactJs-HOL/scorecalculatorapp/src/Components/CalculateScore.js
@@ -0,0 +1,35 @@
+import '../Stylesheets/mystyle.css';
+
+const percentToDecimal = (decimal) => {
+  return (decimal.toFixed(2) + '%');
+};
+
+const calcScore = (total, goal) => {
+  return percentToDecimal((total / goal) * 100);
+};
+
+function CalculateScore({ Name, School, total, goal }) {
+  return (
+    <div className="formastyle">
+      <h1 style={{ color: 'brown', fontWeight: 'bold' }}>Student Details:</h1>
+
+      <div className="Name">
+        <b>Name:</b> <span>{Name}</span>
+      </div>
+
+      <div className="School">
+        <b>School:</b> <span>{School}</span>
+      </div>
+
+      <div className="Total">
+        <b>Total:</b> <span>{total} Marks</span>
+      </div>
+
+      <div className="Score">
+        <b>Score:</b> <span>{calcScore(total, goal)}</span>
+      </div>
+    </div>
+  );
+}
+
+export default CalculateScore;
